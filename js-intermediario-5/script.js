c8ac9973727417e0cd889b8b7cbe4c0d15584cc9
LucasSPRZZ <feliusep@gmail.com>
2024-09-14 14:10:02 -0300

    Primeiro commit

---
+++ js-intermediario-5/script.js
@@ -0,0 +1,12 @@
+function trocarPaginaTo1(){
+    window.location.href = 'index.html'
+}
+function trocarPaginaTo2(){
+    window.location.href = 'index2.html'
+}
+function trocarPaginaTo3(){
+    window.location.href = 'index3.html'
+}
+function trocarPaginaTo4(){
+    window.location.href = 'index4.html'
+}
